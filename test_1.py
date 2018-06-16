ea9922325159ee8e43bea7f091ce0992993fb748
sungmin414 <sungmin3231@gmail.com>
2018-06-16 23:47:43 +0900

    수정

--- test_1.py
+++ test_1.py
@@ -4,6 +4,13 @@ from urllib import parse
 import requests
 from bs4 import BeautifulSoup
 
+
+class Notice:
+    def __init__(self, title, date, view):
+        self.title = title
+        self.date = date
+        self.view = view
+
 file_path = 'data/lol_list.html'
 url = 'http://www.leagueoflegends.co.kr/?m=news&cate=notice'
 
@@ -19,8 +26,20 @@ else:
 soup = BeautifulSoup(html,'lxml')
 
 
-table = soup.select_one('table.request-list')
-tr_list = table.select('td > a')
 
-for a in tr_list:
-    print(f'{a.string}')
+table = soup.select('table.request-list > tbody > tr')
+
+notice_list = list()
+
+for title_list in table:
+    title = title_list.select_one('td.tleft').text
+    date = title_list.select_one('td:nth-of-type(2)').text
+    view = title_list.select_one('td:nth-of-type(3)').text
+    notice_list.append(Notice(title, date, view))
+
+
+for notice in notice_list:
+    print(notice.title)
+    print(notice.date)
+    print(notice.view)
+    print()
